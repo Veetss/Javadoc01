9ca2a81ae05dce365096bec730beb5a581b5f1a1
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:16:05 +0100

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Tester {
+/**
+ * Questa è la mia classe main in cui andrò a stampare le richieste
+ * dell'esercizio.
+ *
+ * @author Vittoria Vassarotti
+ */
+
+public class Tester {
+
+    static String word = " Dear";
+    static String name = "Pino";
+    static String surname = "Daniele";
+
+    /**
+     * questo metodo richiama i metodi presenti nella classe WordGames in modo da poter stampare
+     * Hello Dear Pino Daniele
+     * @param args
+     */
+    public static void main(String[] args) {
+        WordGames obj = new WordGames();
+        System.out.println(obj.addHelloWord(word));
+        System.out.println(obj.getFullName(name, surname));
+    }
 }
