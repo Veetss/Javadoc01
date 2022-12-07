f327e39f7f31dbe263bf824650cfd253e7ee321d
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:16:30 +0100

    Update WordGames.java

--- src/WordGames.java
+++ src/WordGames.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class WordGames {
+/**
+ * Questa è la classe in cui andrò ad inserire i metodi di riferimento da utilizzare
+ * nella classe main.
+ *
+ * @author Vittoria Vassarotti
+ */
+
+public class WordGames {
+
+    /**
+     * questo metodo mi ritorna la stringa Hello sommata alla mia variabile word("Dear")
+     * @param word
+     * @return
+     */
+    public String addHelloWord(String word){
+        return "Hello" + word;
+    }
+
+    /**
+     * questo metodo mi ritorna la somma delle stringhe name("Pino") e surname("Daniele")
+     * @param name
+     * @param surname
+     * @return
+     */
+    public String getFullName(String name, String surname){
+        return name + " " + surname;
+    }
 }
